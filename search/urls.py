7985544db597436474dd96dd6fd07e70712fb584
Acer <chavkin94@gmail.com>
2022-06-14 20:30:03 +0900

    Поиск по всем параметрам

--- search/urls.py
+++ search/urls.py
@@ -6,4 +6,7 @@ app_name = 'organization'
 urlpatterns = [
     path('', search_view, name='search_view'),
     path('search/accounts/', search_accounts, name='search_accounts'),
+    path('search/masters/', search_masters, name='search_masters'),
+    path('search/posts/', search_posts, name='search_posts'),
+    path('search/services/', search_services, name='search_services'),
 ]
